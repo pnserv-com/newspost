f5e2736276032de37911f0b270739311c621aec6
Yoshihisa Tanaka <yoshihisa@iij.ad.jp>
2013-04-16 21:04:18 +0900

    use json file's mtime as updated time

--- newspost/__init__.py
+++ newspost/__init__.py
@@ -5,6 +5,9 @@ import json
 import glob
 import datetime
 from argparse import ArgumentParser
+from ConfigParser import SafeConfigParser
+
+from nckvsclient import KVSClient
 
 
 def expandpath(path):
@@ -20,28 +23,40 @@ def extract(news):
     return [x['article'] for x in sorted(news, key=lambda x: x['seq'])]
 
 
-def register(filename):
+def prepare(filename):
     name = os.path.splitext(os.path.basename(filename))[0]
-    param = {
+    mtime = datetime.datetime.fromtimestamp(os.path.getmtime(filename))
+    return {
         "type": name.split('_')[-1],
         "articles": extract(parse(filename)),
-        "updated_at": datetime.datetime.now().isoformat()
+        "updated_at": mtime.isoformat()
     }
-    print json.dumps(param, ensure_ascii=False)
+
+
+def upsert(nckvs, data):
+    data = [dict(id='-1', **x) for x in data]
+    return nckvs.set(data)
 
 
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument('dir')
+    parser.add_argument('-c', '--config', default='newspost.ini')
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    rule = os.path.join(expandpath(args.dir), '*_char54_*.json')
 
-    for filename in glob.glob(rule):
-        register(filename)
+    parser = SafeConfigParser()
+    parser.read(args.config)
+    config = dict(parser.items('nckvs'))
+    config['datatypeversion'] = int(config.get('datatypeversion', '1'))
+    nckvs = KVSClient(**config)
+
+    rule = os.path.join(expandpath(args.dir), '*_char54_*.json')
+    data = [prepare(x) for x in glob.glob(rule)]
+    print upsert(nckvs, data)
 
 
 if __name__ == '__main__':
